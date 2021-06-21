19e54ea49eb24bddc12b7b403f760cbd94b464d0
Sergey A <sergey.v.akkuzin@gmail.com>
2021-06-21 20:51:16 +0300

    Create tests to synchronize user tasks

---
+++ tests/Services/UserTask/UserTaskSyncTest.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace Stackflows\StackflowsPlugin\Tests\Services\UserTask;
+
+use Psr\Log\LoggerInterface;
+use Stackflows\StackflowsPlugin\Channels\UserTaskChannel;
+use Stackflows\StackflowsPlugin\Exceptions\TooManyErrors;
+use Stackflows\StackflowsPlugin\Services\UserTask\UserTaskSync;
+use PHPUnit\Framework\TestCase;
+use Stackflows\StackflowsPlugin\Services\UserTask\UserTaskSyncInterface;
+use Stackflows\StackflowsPlugin\Tests\Factories\UserTaskFactory;
+
+class UserTaskSyncTest extends TestCase
+{
+    /** @test */
+    public function handle()
+    {
+        $task = UserTaskFactory::new()->create();
+        $sync = $this->createMock(UserTaskSyncInterface::class);
+        $sync->expects($this->once())
+            ->method('sync')
+            ->with(
+                $this->equalTo([$task]),
+                $this->equalTo(['createdAt' => null])
+            );
+
+        $api = $this->createMock(UserTaskChannel::class);
+        $api->expects($this->once())
+            ->method('getList')
+            ->willReturn([$task]);
+
+        $logger = $this->createMock(LoggerInterface::class);
+
+        $syncHandler = new UserTaskSync($api, $logger, [$sync]);
+
+        $syncHandler->handle();
+    }
+
+    /** @test */
+    public function handleWithLogError()
+    {
+        $task = UserTaskFactory::new()->create();
+        $sync = $this->createMock(UserTaskSyncInterface::class);
+        $sync->expects($this->once())
+            ->method('sync')
+            ->willThrowException(new \Exception("oops"));
+
+        $api = $this->createMock(UserTaskChannel::class);
+        $api->expects($this->once())
+            ->method('getList')
+            ->willReturn([$task]);
+
+        $logger = $this->createMock(LoggerInterface::class);
+        $logger->expects($this->once())
+            ->method('error');
+
+        $syncHandler = new UserTaskSync($api, $logger, [$sync]);
+
+        $syncHandler->handle();
+    }
+
+    /** @test */
+    public function handleWithTooManyError()
+    {
+        $task = UserTaskFactory::new()->create();
+        $sync = $this->createMock(UserTaskSyncInterface::class);
+        $sync->method('sync')
+            ->willThrowException(new \Exception("oops"));
+
+        $api = $this->createMock(UserTaskChannel::class);
+        $api->method('getList')
+            ->willReturn([$task]);
+
+        $logger = $this->createMock(LoggerInterface::class);
+
+        $syncHandler = new UserTaskSync($api, $logger, [$sync]);
+
+        $this->expectException(TooManyErrors::class);
+
+        for ($i = 0; $i < 7; $i++) {
+            $syncHandler->handle();
+        }
+    }
+
+    /** @test */
+    public function handleWithCreatedAfterAssert()
+    {
+        $task = UserTaskFactory::new()->create();
+        $sync = $this->createMock(UserTaskSyncInterface::class);
+
+        $api = $this->createMock(UserTaskChannel::class);
+        $api->method('getList')
+            ->willReturn([$task]);
+
+        $logger = $this->createMock(LoggerInterface::class);
+
+        $syncHandler = new UserTaskSync($api, $logger, [$sync]);
+
+        $syncHandler->handle();
+
+        $api->expects($this->once())
+            ->method('getList')
+            ->with($this->equalToWithDelta(new \DateTime(), 2));
+
+        $syncHandler->handle();
+    }
+}
