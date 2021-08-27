99a46e6fcd2310e3c63808a783e5f2ea664a32d1
Simonas Šerlinskas <saimaz@users.noreply.github.com>
2021-08-27 14:51:31 +0300

    remove note necessary configuration class again

--- src/Stackflows.php
+++ src/Stackflows.php
@@ -3,22 +3,14 @@
 namespace Stackflows\StackflowsPlugin;
 
 use Stackflows\StackflowsPlugin\Http\Client\GatewayClient;
-use StackflowsPlugin\StackflowsConfiguration;
 
 class Stackflows
 {
-    private StackflowsConfiguration $configuration;
     private GatewayClient $gatewayClient;
 
-    public function __construct(StackflowsConfiguration $configuration, GatewayClient $gatewayClient)
+    public function __construct(GatewayClient $gatewayClient)
     {
-        $this->configuration = $configuration;
         $this->gatewayClient = $gatewayClient;
-    }
-
-    public function getConfiguration(): StackflowsConfiguration
-    {
-        return $this->configuration;
     }
 
     public function getGatewayClient(): GatewayClient
